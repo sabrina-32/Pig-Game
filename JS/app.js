4e865ed877fac060c4e57ec5635e688accb2331b
Sabrina Nusrat Ayvi <sabrina.ayvi@selise.com.bd>
2020-01-14 18:52:35 +0600

    event handling

--- JS/app.js
+++ JS/app.js
@@ -11,21 +11,51 @@ GAME RULES:
 
 
 
-var scorers, roundScore, activePlayer, dice;
+var scorers, roundScore, activePlayer;
 
 scores = [0,0];
 roundScore =0;
 activePlayer  =1;
 
-dice = Math.floor(Math.random()*6)+1;
-
-document.querySelector('#current-' + activePlayer).textContent = dice;
-
-var x = document.querySelector('#score-0').textContent;
-
-console.log(x);
 
 document.querySelector('.dice').style.display = 'none';
 
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+ 
+
+document.querySelector('.btn-roll').addEventListener('click',  function(){
+    //  1. random num,
+
+  var  dice = Math.floor(Math.random()*6)+1;
 
 
+// 2. display  the  result
+
+var diceDOM = document.querySelector('.dice');
+
+diceDOM.style.display = 'block';
+diceDOM.src = 'dice-' + dice + '.png';
+
+
+
+
+
+//3. update the round score if  the rolled  num was  not  a 1
+
+
+
+
+
+});
+
+
+
+//document.querySelector('#current-' + activePlayer).textContent = dice;
+
+// var x = document.querySelector('#score-0').textContent;
+
+// console.log(x);
