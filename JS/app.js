0f68913d6e1210cf942f2249fc0b1f0a52518797
Sabrina Nusrat Ayvi <sabrina.ayvi@selise.com.bd>
2020-01-15 12:52:37 +0600

    game initializing

--- JS/app.js
+++ JS/app.js
@@ -4,17 +4,9 @@
 
 var scores, roundScore, activePlayer;
 
-scores = [0,0];
-roundScore =0;
-activePlayer  =0;
+init();
 
 
-document.querySelector('.dice').style.display = 'none';
-
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
 
  
 
@@ -113,6 +105,37 @@ function nextPlayer(){
   
 }
 
+//new  game button
+document.querySelector('.btn-new').addEventListener('click',init);
 
 
+
+function init(){
+
+  
+scores = [0,0];
+roundScore =0;
+activePlayer  =0;
+
+var gamePlaying = true;
+
+
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.getElementById('name-0').textContent = 'Player 1';
+document.getElementById('name-1').textContent = 'Player 2';
+
+document.querySelector('.player-1-panel').classList.remove = ('Winner');
+document.querySelector('.player-0-panel').classList.remove = ('Winner');
+
+document.querySelector('.player-0-panel').classList.remove = ('active');
+document.querySelector('.player-1-panel').classList.remove = ('Winner');
+document.querySelector('.player-0-panel').classList.add = ('active');
+
+}
 
